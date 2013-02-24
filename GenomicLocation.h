0b1e49972a53286240db873289f9398b21efdcc9
Yi Qiao <atrusqiao@gmail.com>
2013-02-23 22:59:24 -0500

    Included two data structure for genomic locations

---
+++ GenomicLocation.h
@@ -0,0 +1,27 @@
+#ifndef GENOMIC_LOCATION_H
+#define GENOMIC_LOCATION_H
+
+/**
+ * @file GenomicLocation.h
+ * Interface description of the helper class GenomicLocation
+ *
+ * @author Yi Qiao
+ */
+
+namespace SubcloneExplorer {
+
+	/**
+	 * @brief A point on a reference genome
+	 *
+	 * This class represents a point location on a (implied) reference genome.
+	 * It consists of a chromosome id, and a position.
+	 *
+	 * The position is 0 based
+	 */
+	class GenomicLocation {
+		public:
+			int chrom; /**< The integer id of the chromosome */
+			unsigned long position; /**< The 0-based position */
+	};
+};
+#endif
---
+++ GenomicRange.h
@@ -0,0 +1,32 @@
+#ifndef GENOMIC_RANGE_H
+#define GENOMIC_RANGE_H
+
+#include "GenomicLocation.h"
+
+/**
+ * @file GenomicRange.h
+ * Interface description of the helper class GenomicRange
+ *
+ * @author Yi Qiao
+ */
+
+namespace SubcloneExplorer {
+
+	/**
+	 * @brief A segment on a reference genome
+	 *
+	 * This class represents a segment on a (implied) reference genome.
+	 * It contains a start position described by GenomicLocation, and 
+	 * a greater than 1 length. In this way a segment will never cross
+	 * a chromosome boundry.
+	 *
+	 * @see GenomicLocation
+	 */
+	class GenomicRange {
+		public:
+			GenomicLocation start;	/**< The starting position of the segment */
+			unsigned long length;	/**< The length of the segment */
+	};
+};
+
+#endif
