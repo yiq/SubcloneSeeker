6a498569024fa581716046a2e593cdf7e19eb16a
Yi Qiao <atrusqiao@gmail.com>
2013-07-30 16:58:45 -0400

    Included license info for all sources

--- utils/segtxt2db.cc
+++ utils/segtxt2db.cc
@@ -6,6 +6,29 @@
  * @author Yi Qiao
  */
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Yi Qiao
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
 #include <iostream>
 #include <fstream>
 #include <cmath>
@@ -196,20 +219,6 @@ int main(int argc, char* argv[]) {
 	// Cluster the CNVs based on ratio
 	// *******************************
 	std::vector<EventCluster *> clusters = EventCluster::clustering(events, _threshold);
-
-	std::ofstream eventsFile;
-	eventsFile.open("events.seg.txt");
-	eventsFile<<"ID\tchrom\tstart.loc\tend.loc\tnum.marks\tseg.mean"<<std::endl;
-	for(size_t i=0; i<events.size(); i++) {
-		CNV* theEvent = dynamic_cast<CNV*>(events[i]);
-		if(theEvent == NULL)
-			continue;
-		eventsFile<<"clustered\t"<<theEvent->range.chrom<<"\t"<<theEvent->range.position<<"\t"<<theEvent->range.position + theEvent->range.length;
-		eventsFile<<"\t"<<theEvent->range.length<<"\t"<<log2(theEvent->frequency)<<std::endl;
-	}
-
-	eventsFile.close();
-
 
 	// ************************************************
 	// Correct the clusters by purity and neutral level
