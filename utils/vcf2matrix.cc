3e356727125d8102e086f301cf41b60d5970ee92
Yi Qiao <atrusqiao@gmail.com>
2021-03-30 17:06:09 -0600

    introduced htslibpp as a dependency; and created a new target to test

---
+++ utils/vcf2matrix.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <htslibpp/htslibpp.h>
+#include <htslibpp/htslibpp_variant.h>
+
+using namespace YiCppLib::HTSLibpp;
+
+int main(int argc, char** argv) {
+
+    auto vcf = htsOpen(argv[1], "r");
+    auto header = htsHeader<bcfHeader>::read(vcf);
+
+    for(auto it = htsHeader<bcfHeader>::dictBegin(header, htsHeader<bcfHeader>::DictType::ID); it != htsHeader<bcfHeader>::dictEnd(header, htsHeader<bcfHeader>::DictType::ID); it++) {
+        const auto proxy = htsProxy(*it);
+        std::cout<<proxy.key()<<std::endl;
+    }
+
+    return 0;
+}
